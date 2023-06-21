9a8d08b2f4ea510f98939e78d68b7ece607379b2
darina bebenina <darinabebenina@yahoo.com>
2023-06-22 00:50:49 +0500

    tariffs begining

---
+++ tariffs/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+import styles from './components/Tariff.css';
+import Tariff from './components/Tariff';
+import { tariffs } from './tariffsData.js';
+
+function App() {
+  return (
+    <div className="App">
+      {
+        tariffs.map((tariff, index) =>
+          <Tariff key="index" name={tariff.name} price={tariff.price} speed={tariff.price} features={tariff.features} />
+        )
+      }
+    </div>
+  );
+}
+
+export default App;
---
+++ tariffs/src/components/Tariff.jsx
@@ -0,0 +1,16 @@
+import styles from './Tariff.css';
+
+function Tariff(props) {
+    const { name, price, speed, features, color } = props;
+
+    return (
+        <div className="tariff-card">
+            <div className="tariff-name">{name}</div>
+            <div className="tariff-price">руб {price} /мес</div>
+            <div className="tariff-speed">до {speed} Mbit/сек</div>
+            <div className="tariff-features">{features}</div>
+        </div>
+    )
+}
+
+export default Tariff;
