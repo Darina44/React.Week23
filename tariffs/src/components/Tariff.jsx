cc87b4c761e170dbb2963127fbcb4f7f88b469aa
darina bebenina <darinabebenina@yahoo.com>
2023-06-30 00:42:13 +0500

    useState

--- tariffs/src/components/Tariff.jsx
+++ tariffs/src/components/Tariff.jsx
@@ -1,15 +1,25 @@
+import React, { useState } from 'react';
 
 function Tariff(props) {
+
     const { name, price, speed, features, color, isSelected } = props;
+
+    const [selected, setSelected] = useState(false);
+
+    const handleChange = () => {
+        setSelected(!selected);
+    };
+
     let tariffSelected = "";
-    if (isSelected) tariffSelected = "selected";
+    if (selected) tariffSelected = "selected";
     return (
-        <div className={`tariff-card ${color} ${tariffSelected}`} >
+        <div className={`tariff-card ${color} ${tariffSelected}`}
+            onClick={handleChange}>
             <div className="tariff-name">{name}</div>
             <div className="tariff-price">{price}</div>
             <div className="tariff-speed">до {speed} Mbit/сек</div>
             <div className="tariff-features">{features}</div>
-        </div>
+        </div >
     )
 }
 
